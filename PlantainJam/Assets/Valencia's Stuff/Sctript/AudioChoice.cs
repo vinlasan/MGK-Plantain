14982ff46cb57f6050ad1bb9ad57e658c7fe23a5
Vincent LaSane <vinlasan@ourlook.com>
2021-10-01 16:34:42 -0700

    Audio Choice making event calls for the MixerController

--- PlantainJam/Assets/Valencia's Stuff/Sctript/AudioChoice.cs
+++ PlantainJam/Assets/Valencia's Stuff/Sctript/AudioChoice.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Gameplay;
 
 public class AudioChoice : MonoBehaviour
 {
@@ -19,66 +20,73 @@ public class AudioChoice : MonoBehaviour
         {
             case "Piano":
                 Debug.Log("Playing Piano");
-                if (jazz.isPlaying || dig.isPlaying || organ.isPlaying || country.isPlaying)
+                /*if (jazz.isPlaying || dig.isPlaying || organ.isPlaying || country.isPlaying)
                 {
                     jazz.Stop();
                     dig.Stop();
                     organ.Stop();
                     country.Stop();
                 }
-                piano.Play();
+                piano.Play();*/
+                
+                EventManager.OnAudioPlayRecordMusic(piano);
                 break;
             case "Jazz":
                 Debug.Log("Playing Jazz");
-                if (piano.isPlaying || dig.isPlaying || organ.isPlaying || country.isPlaying)
+                /*if (piano.isPlaying || dig.isPlaying || organ.isPlaying || country.isPlaying)
                 {
                     piano.Stop();
                     dig.Stop();
                     organ.Stop();
                     country.Stop();
                 }
-                jazz.Play();
+                jazz.Play();*/
+                EventManager.OnAudioPlayRecordMusic(jazz);
                 break;
             case "Dig":
                 Debug.Log("Playing Dig");
-                if (jazz.isPlaying || piano.isPlaying || organ.isPlaying || country.isPlaying)
+                /*if (jazz.isPlaying || piano.isPlaying || organ.isPlaying || country.isPlaying)
                 {
                     jazz.Stop();
                     piano.Stop();
                     organ.Stop();
                     country.Stop();
                 }
-                dig.Play();
+                dig.Play();*/
+                EventManager.OnAudioPlayRecordMusic(dig);
                 break;
             case "Organ":
                 Debug.Log("Playing Organ");
-                if (jazz.isPlaying || dig.isPlaying || piano.isPlaying || country.isPlaying)
+               /* if (jazz.isPlaying || dig.isPlaying || piano.isPlaying || country.isPlaying)
                 {
                     jazz.Stop();
                     dig.Stop();
                     piano.Stop();
                     country.Stop();
                 }
-                organ.Play();
+                organ.Play();*/
+                EventManager.OnAudioPlayRecordMusic(organ);
                 break;
             case "Country":
                 Debug.Log("Playing Country");
-                if (jazz.isPlaying || dig.isPlaying || organ.isPlaying || piano.isPlaying)
+                /*if (jazz.isPlaying || dig.isPlaying || organ.isPlaying || piano.isPlaying)
                 {
                     jazz.Stop();
                     dig.Stop();
                     organ.Stop();
                     piano.Stop();
                 }
-                country.Play();
+                country.Play();*/
+                EventManager.OnAudioPlayRecordMusic(country);
                 break;
             case "No":
                 Debug.Log("Player choose No");
-                piano.Stop();
+               /* piano.Stop();
                 jazz.Stop();
                 dig.Stop();
                 organ.Stop();
-                country.Stop();
+                country.Stop();*/
+                EventManager.OnAudioStopRecordMusic();
                 recordPuzzle.ResetStory();
             break;
             case "Yes":
@@ -86,12 +94,14 @@ public class AudioChoice : MonoBehaviour
                 {
                     Debug.Log("Correct Choice");
                     correct = true;
+                    EventManager.OnAudioStopRecordMusic();
                 }
                 else if (piano.isPlaying || dig.isPlaying || organ.isPlaying || country.isPlaying)
                 {
                     Debug.Log("Wrong Choice");
                     correct = false;
                     errorScript.increaseErrrorCount();
+                    EventManager.OnAudioStopRecordMusic();
                     recordPuzzle.ResetStory();
                 }
                 break;
